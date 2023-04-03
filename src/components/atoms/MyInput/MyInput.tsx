5e269bc0401c2d37064eb8e84041c1238b7a70d5
Chanho Won <wonch30@gmail.com>
2023-04-03 17:03:59 +0900

    feature: MyInput - 기본 디자인 구현 완료 - props = {value, inputHandler, isClearShow}   - value: 입력값   - inputHandler: 입력값 변경 함수(setState)   - isClearShow: 입력값 초기화 버튼 활용 여부 (true or false)

--- src/components/atoms/MyInput/MyInput.tsx
+++ src/components/atoms/MyInput/MyInput.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import styles from "./MyInput.module.css";
 
-export default function MyInput() {
-  return <div>MyInput</div>;
+type inputProps = {
+  value: string | number;
+  inputHandler: React.Dispatch<React.SetStateAction<string>>;
+  isClearShow?: boolean;
+};
+
+export default function MyInput({ value, inputHandler, isClearShow = false }: inputProps) {
+  return (
+    <>
+      <div className="w-full flex">
+        <input
+          id="inputValue"
+          className={styles.inputBox}
+          value={value}
+          onChange={(e) => inputHandler(e.target.value)}
+        />
+        {isClearShow ? <button onClick={() => inputHandler("")}>초기화</button> : null}
+      </div>
+    </>
+  );
 }
