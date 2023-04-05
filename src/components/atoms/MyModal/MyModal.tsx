2314e5e250f46ea08b68de7209c94be47a460137
hailey <hallov012@gmail.com>
2023-04-05 21:28:40 +0900

    refactor: modal 조건문 binding에서 portal로 변경

--- src/components/atoms/MyModal/MyModal.tsx
+++ src/components/atoms/MyModal/MyModal.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./MyModal.module.css";
+import ReactDOM from "react-dom";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 
 interface Props {
@@ -9,32 +10,29 @@ interface Props {
 }
 
 export default function MyModal({ isOpen, closeModal, children }: Props) {
+  if (!isOpen) {
+    return null;
+  }
+
   const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.target === e.currentTarget) {
       closeModal();
     }
   };
 
-  return (
-    <>
-      {isOpen && (
-        <div
-          className={styles.modalOverlay}
-          onClick={handleOverlayClick}
-          data-testid="modalOverlay"
-        >
-          <div className="bg-back rounded-lg px-5 py-5">
-            <div className="flex justify-end mb-3">
-              <XMarkIcon
-                className="w-6 h-6 cursor-pointer"
-                onClick={closeModal}
-                data-testid="xMarkIcon"
-              />
-            </div>
-            <div className="flex flex-col items-center justify-center">{children}</div>
-          </div>
+  return ReactDOM.createPortal(
+    <div className={styles.modalOverlay} onClick={handleOverlayClick} data-testid="modalOverlay">
+      <div className="bg-back rounded-lg px-5 py-5">
+        <div className="flex justify-end mb-3">
+          <XMarkIcon
+            className="w-6 h-6 cursor-pointer"
+            onClick={closeModal}
+            data-testid="xMarkIcon"
+          />
         </div>
-      )}
-    </>
+        <div className="flex flex-col items-center justify-center">{children}</div>
+      </div>
+    </div>,
+    document.getElementById("modal-root") as Element,
   );
 }
