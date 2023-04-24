b1c0bdd8d18329d6ddb488bca01255c80a53ed30
Chanho Won <wonch30@gmail.com>
2023-04-24 17:34:55 +0900

    feat: MyText - [x] myText 코드 가독성 향상

--- src/components/atoms/MyText/MyText.tsx
+++ src/components/atoms/MyText/MyText.tsx
@@ -1,10 +1,27 @@
 import React from "react";
-import styels from "./MyText.module.css";
+import styles from "./MyText.module.css";
 
 interface myTextProps {
   value: string;
+  size?: "sm" | "md" | "lg";
 }
 
-export default function MyText({ value }: myTextProps) {
-  return <div className={styels.myTextBox}>{value}</div>;
+const frameStyle = {
+  sm: `${styles.myTextBox} p-1 pl-2`,
+  md: `${styles.myTextBox} p-2 pl-2`,
+  lg: `${styles.myTextBox} p-3 pl-2`,
+};
+
+const sizes = {
+  sm: "text-[0.5rem]",
+  md: "text-[1.0rem]",
+  lg: "text-[1.5rem]",
+};
+
+export default function MyText({ value, size = "md" }: myTextProps) {
+  return (
+    <div className={frameStyle[size]}>
+      <span className={sizes[size]}>{value}</span>
+    </div>
+  );
 }
