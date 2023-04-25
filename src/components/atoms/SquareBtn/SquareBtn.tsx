e179abb233436d7f2950536c471f4dbead9179c7
jina jeong <jina092400@gmail.com>
2023-04-25 21:41:10 +0900

    ADD | sqaureBtn atom 생성

--- src/components/atoms/SquareBtn/SquareBtn.tsx
+++ src/components/atoms/SquareBtn/SquareBtn.tsx
@@ -2,27 +2,36 @@ import React from "react";
 import styles from "./SquareBtn.module.css";
 
 interface SquareBtnProps {
-  // width?: number;
-  // height?: number;
-  theme: string;
-  fontSize?: string;
+  theme: "myPage" | "comments" | "commentReport" | "commentLike" | "gameReport" | "gameLike";
   isSelected?: boolean;
   children: React.ReactNode;
 }
 
-export default function SquareBtn({
-  // width,
-  // height,
-  theme,
-  fontSize = "s",
-  isSelected = false,
-  children,
-}: SquareBtnProps) {
+const getStyle = ({ theme, isSelected }: SquareBtnProps) => {
+  switch (theme) {
+    case "myPage":
+      return `text-main rounded-[5px] px-4 py-1`;
+    case "comments":
+      return `text-main rounded-[21px] px-4 py-1`;
+    case "commentReport":
+      return `text-font2 rounded-[5px] p-2`;
+    case "commentLike":
+      return `rounded-[5px] p-2 ${isSelected ? `text-pink-900` : `text-font2`}`;
+    case "gameReport":
+      return `rounded-[5px] p-2`;
+    case "gameLike":
+      return `rounded-[5px] p-2 ${isSelected ? `text-pink-900` : `text-main`}`;
+  }
+};
+
+export default function SquareBtn(props: SquareBtnProps) {
   return (
-    <>
-      <div className={`flex justify-center ${styles.squareBtnShadow}`}>
-        <div className="p-2 items-center text-center">{children}</div>
-      </div>
-    </>
+    <div
+      className={`flex justify-center ${getStyle(props)} ${
+        props.isSelected ? `${styles.selectedSquareBtn}` : `${styles.squareBtn}`
+      }`}
+    >
+      <div className="p-2 items-center text-center font-suitMedium">{props.children}</div>
+    </div>
   );
 }
