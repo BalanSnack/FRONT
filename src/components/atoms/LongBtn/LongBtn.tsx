7fdb3d79bbe149553591d1f54e6e608dc04d6b9d
jina jeong <jina092400@gmail.com>
2023-04-25 17:32:31 +0900

    Update | LongBtn className 변수화

--- src/components/atoms/LongBtn/LongBtn.tsx
+++ src/components/atoms/LongBtn/LongBtn.tsx
@@ -2,31 +2,24 @@ import React, { useState, useEffect } from "react";
 import styles from "./LongBtn.module.css";
 
 interface LongBtnProps {
-  theme?: string;
+  theme: "basic" | "google" | "kakao";
   isSelected?: boolean;
   children: React.ReactNode;
 }
 
-export default function LongBtn({ theme, isSelected = false, children }: LongBtnProps) {
+const getStyle = ({ theme, isSelected }: LongBtnProps) => {
+  const colors = {
+    basic: `bg-back ${isSelected ? `${styles.selectedLongBtn}` : `${styles.longBtn}`}`,
+    google: `bg-[#FFFFFF] ${styles.longBtn}`,
+    kakao: `bg-[#F7E600] ${styles.longBtn}`,
+  };
+  return colors[theme];
+};
+
+export default function LongBtn(props: LongBtnProps) {
   return (
-    <>
-      {theme === "basic" && (
-        <div
-          className={`bg-back ${isSelected ? `${styles.selectedLongBtn}` : `${styles.longBtn}`}`}
-        >
-          <div className="">{children}</div>
-        </div>
-      )}
-      {theme === "google" && (
-        <div className={`bg-[#FFFFFF] ${styles.longBtn}`}>
-          <div>{children}</div>
-        </div>
-      )}
-      {theme === "kakao" && (
-        <div className={`bg-[#F7E600] ${styles.longBtn}`}>
-          <div>{children}</div>
-        </div>
-      )}
-    </>
+    <div className={getStyle(props)}>
+      <div>{props.children}</div>
+    </div>
   );
 }
