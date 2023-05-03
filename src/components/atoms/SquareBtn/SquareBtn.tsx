e87438d7f94ecaf3378499d8a2bdef81b8fb3e28
jina jeong <jina092400@gmail.com>
2023-05-03 18:17:36 +0900

    Update | SquareBtn 드래그 방지 적용, icon 추가

--- src/components/atoms/SquareBtn/SquareBtn.tsx
+++ src/components/atoms/SquareBtn/SquareBtn.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import styles from "./SquareBtn.module.css";
 
+import { HeartIcon } from "@heroicons/react/24/solid";
+import { ExclamationCircleIcon } from "@heroicons/react/24/outline";
+
 interface SquareBtnProps {
   theme: "myPage" | "comments" | "commentReport" | "commentLike" | "gameReport" | "gameLike";
+  text?: string;
+  icon?: string;
   isSelected?: boolean;
   children: React.ReactNode;
 }
 
-const getStyle = ({ theme, isSelected }: SquareBtnProps) => {
+const getStyle = ({ theme, isSelected = false }: SquareBtnProps) => {
   switch (theme) {
     case "myPage":
       return `text-main rounded-[5px] px-4 py-1`;
@@ -18,20 +23,24 @@ const getStyle = ({ theme, isSelected }: SquareBtnProps) => {
     case "commentLike":
       return `rounded-[5px] p-2 ${isSelected ? `text-pink-900` : `text-font2`}`;
     case "gameReport":
-      return `rounded-[5px] p-2`;
+      return `rounded-[5px] p-2 text-main`;
     case "gameLike":
-      return `rounded-[5px] p-2 ${isSelected ? `text-pink-900` : `text-main`}`;
+      return `rounded-[5px] p-2 ${isSelected ? `text-pink-900` : `text-font2`}`;
   }
 };
 
 export default function SquareBtn(props: SquareBtnProps) {
   return (
     <div
-      className={`flex justify-center ${getStyle(props)} ${
+      className={`flex justify-center cursor-pointer ${styles.banSelect} ${getStyle(props)} ${
         props.isSelected ? `${styles.selectedSquareBtn}` : `${styles.squareBtn}`
       }`}
     >
-      <div className="p-2 items-center text-center font-suitMedium">{props.children}</div>
+      <div className="p-2 items-center flex text-center font-suitMedium">
+        {props.icon === "report" && <ExclamationCircleIcon className="w-6 h-6" />}
+        {props.icon === "heart" && <HeartIcon className="w-6 h-6" />}
+        {props.text}
+      </div>
     </div>
   );
 }
