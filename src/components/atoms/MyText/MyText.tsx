0735e4ea64c2a64f875f320a812dcec512e69ac5
Chanho Won <wonch30@gmail.com>
2023-05-03 16:27:20 +0900

    faeture: MyText - [x] 배경 제거(div, css module의 frame 영역 삭제) - [x] props.value로 받던 text 내용을 props.children으로 받도록 변경

--- src/components/atoms/MyText/MyText.tsx
+++ src/components/atoms/MyText/MyText.tsx
@@ -1,20 +1,13 @@
 import React from "react";
-import styles from "./MyText.module.css";
 
 type sizeType = "sm" | "md" | "lg";
 type colorType = "main" | "pink" | "blue" | "font1" | "font2";
 
 interface myTextProps {
-  value: string;
+  children: React.ReactNode;
   size?: sizeType;
   color?: colorType;
 }
-
-const frameStyle = {
-  sm: `${styles.myTextBox} p-1 pl-2`,
-  md: `${styles.myTextBox} p-2 pl-2`,
-  lg: `${styles.myTextBox} p-3 pl-2`,
-};
 
 const sizes = {
   sm: "text-[0.5rem]",
@@ -34,10 +27,6 @@ const getFontStyle = (size: sizeType, color: colorType) => {
   return `${fontColor[color]} ${sizes[size]}`;
 };
 
-export default function MyText({ value, size = "md", color = "font1" }: myTextProps) {
-  return (
-    <div className={frameStyle[size]}>
-      <span className={getFontStyle(size, color)}>{value}</span>
-    </div>
-  );
+export default function MyText({ children, size = "md", color = "font1" }: myTextProps) {
+  return <span className={getFontStyle(size, color)}>{children}</span>;
 }
