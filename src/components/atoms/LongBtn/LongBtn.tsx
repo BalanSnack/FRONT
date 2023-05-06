96e84dff810bf33dce388f3dfb6c814c53348d60
jina jeong <jina092400@gmail.com>
2023-05-06 22:50:34 +0900

    Update | long-btn 카카오 로그인 아이콘 및 스타일 추가

--- src/components/atoms/LongBtn/LongBtn.tsx
+++ src/components/atoms/LongBtn/LongBtn.tsx
@@ -1,25 +1,30 @@
 import React, { useState, useEffect } from "react";
 import styles from "./LongBtn.module.css";
 
+import { ChatBubbleOvalLeftIcon } from "@heroicons/react/24/solid";
+
 interface LongBtnProps {
   theme: "basic" | "google" | "kakao";
   isSelected?: boolean;
   children: React.ReactNode;
+  text: string;
+  icon?: string;
 }
 
 const getStyle = ({ theme, isSelected }: LongBtnProps) => {
   const colors = {
     basic: `bg-back ${isSelected ? `${styles.selectedLongBtn}` : `${styles.longBtn}`}`,
     google: `bg-[#FFFFFF] ${styles.longBtn}`,
-    kakao: `bg-[#F7E600] ${styles.longBtn}`,
+    kakao: `bg-[#FEE500] text-[#191919] font-suitBold ${styles.longBtn}`,
   };
   return colors[theme];
 };
 
 export default function LongBtn(props: LongBtnProps) {
   return (
-    <div className={getStyle(props)}>
-      <div>{props.children}</div>
+    <div className={`flex justify-center gap-3 ${getStyle(props)}`}>
+      {props.icon === "kakao" && <ChatBubbleOvalLeftIcon className="w-6 h-6 opacity-90" />}
+      <div>{props.text}</div>
     </div>
   );
 }
