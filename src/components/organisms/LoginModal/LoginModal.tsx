da55f7bf72bbc0b44e263d733b1698b2cde45187
hailey <hallov012@gmail.com>
2023-05-29 23:44:06 +0900

    feat: LoginModal

---
+++ src/components/organisms/LoginModal/LoginModal.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import MyModal from "@/components/atoms/MyModal/MyModal";
+import MyText from "@/components/atoms/MyText/MyText";
+import LongBtn from "@/components/atoms/LongBtn/LongBtn";
+
+interface Props {
+  isOpen: boolean;
+  closeModal: () => void;
+}
+
+export default function LoginModal({ isOpen, closeModal }: Props) {
+  return (
+    <MyModal isOpen={isOpen} closeModal={closeModal}>
+      <MyText size="lg" weight="semi" color="font1">
+        로그인
+      </MyText>
+      <div className="flex flex-col gap-3 my-10">
+        <LongBtn theme="google" />
+        <LongBtn theme="kakao" />
+      </div>
+      <div className="flex flex-col items-center">
+        <MyText size="sm">By creating an account, you agree to BalanSnack Terms of Use,</MyText>
+        <MyText size="sm">Privacy Policy and Cookie Policy</MyText>
+      </div>
+    </MyModal>
+  );
+}
