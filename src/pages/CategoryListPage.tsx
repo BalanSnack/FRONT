380e0ea9476b967e94d8947610dd335d6dfa4de2
hyelim <hallov012@gmail.com>
2023-06-06 21:19:02 +0900

    feat: pagination

--- src/pages/CategoryListPage.tsx
+++ src/pages/CategoryListPage.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Pagination from "@/components/molecules/Pagination/Pagination";
+import { useState } from "react";
 
 export default function CategoryListPage() {
+  const [page, setPage] = useState<number>(1);
+
+  const pageHandler = (pageNum: number) => {
+    setPage(pageNum);
+  };
+
   return (
     <div>
       <h1>카테고리 리스트 페이지</h1>
+      <Pagination fullPage={15} page={page} pageHandler={pageHandler} />
     </div>
   );
 }
