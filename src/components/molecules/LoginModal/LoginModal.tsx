803312dddb9117a08efa9d48cbe798bbf6452c95
hyelim <hallov012@gmail.com>
2023-06-28 02:55:02 +0900

    feat: reportModal refactoring and add reason

--- src/components/molecules/LoginModal/LoginModal.tsx
+++ src/components/molecules/LoginModal/LoginModal.tsx
@@ -14,14 +14,16 @@ export default function LoginModal({ isOpen, closeModal }: Props) {
       <MyText size="lg" weight="semi" color="font1">
         로그인
       </MyText>
-      <div className="flex flex-col gap-3 my-10 w-full">
+      <div className="flex flex-col gap-3 mt-5 w-full">
         <LongBtn theme="google" />
         <LongBtn theme="kakao" />
       </div>
-      <div className="flex flex-col items-center">
-        <MyText size="sm">By creating an account, you agree to BalanSnack Terms of Use,</MyText>
-        <MyText size="sm">Privacy Policy and Cookie Policy</MyText>
-      </div>
+      {/* <div className="flex flex-col items-center text-center">
+        <MyText size="sm">
+          By creating an account, you agree to BalanSnack Terms of Use, Privacy Policy and Cookie
+          Policy
+        </MyText>
+      </div> */}
     </MyModal>
   );
 }
